143ba53a49e1fae27c8dd1f5b582ebd7b8747557
Michael Bass <littlebass09>
2026-01-29 10:41:54 -0800

    MB Initial push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ArcticFoxDailyChallenge_1_29_2026
+
+Follow the steps in hamster_run.py to determine which hamster the challenge is looking for!
---
+++ hamster_run.py
@@ -0,0 +1,14 @@
+
+
+## Kits you might want to use: Data, View, Filter, ColumnMin, Sort
+
+# Load the hamsters.csv
+
+# view it
+
+# we only want hamsters that finished the maze
+
+# we also only want hamsters that found the cheddar and swiss, but not the gouda
+
+# which of these hamsters was the fastest?
+
